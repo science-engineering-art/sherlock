6f4cafe5936c9c08c6f79253385f97ef0d492ff2
leandro-driguez <leandro__rodriguez@outlook.com>
2022-11-05 18:51:58 -0400

    refactor(vector): separate the tf calculates

--- src/models/vector_model.py
+++ src/models/vector_model.py
@@ -1,5 +1,7 @@
 from math import log
+import re
 from typing import Dict, List
+from unidecode import unidecode
 from models.base_model import BaseModel
 from models.search_item import SearchItem
 
@@ -12,53 +14,68 @@ class VectorModel(BaseModel):
         self.dict_terms = {}
         self.dict_docs = {}
         self.frequency: List[List[int]] = []
-        self.tf: List[List[float]] = []
+        self.tfs: List[List[float]] = []
         self.idf: List[List[float]] = []
         self.weights: List[List[float]] = []
         
         self.__calculate_weights()
     
     def search(self, query: str) -> List[SearchItem]:
-        pass
+        
+        query_vector: List[str] = [ unidecode(word.lower()) for word in 
+                                   re.findall(r"[\w']+", query) ]
 
-    def __calculate_tf(self):
+        dict_query = {}
 
-        amount_terms = 0; amount_docs=0
-        maxs = [] # maximum frequency of terms in the document
+
+    def __calculate_tf(
+            text: List[str], 
+            dict_terms: Dict[str, int] = {},
+            frequency: List[int] = [],
+            tf: List[float] = []
+        ):
+
+        amount_terms = len(frequency)
+        max_freq = -1
+
+        for word in text:
+
+            if not word in dict_terms:
+                frequency.append(0)
+                dict_terms[word] = amount_terms
+                amount_terms += 1
+                
+            frequency[dict_terms[word]] += 1
+            max_freq = max(max_freq, frequency[dict_terms[word]])
+
+        for j in range(0, amount_terms):
+            tf.append(frequency[j] / max_freq)
+
+    def __calculate_tfs(self):
+
+        amount_terms = 0; amount_docs = 0
 
         for doc in self.corpus:
 
             self.dict_docs[doc] = amount_docs
             self.frequency.append([0 for _ in range(0, amount_terms)])
-            max_frequency_terms = -1
+            self.tfs.append([])
 
-            for word in doc.text:
-            
-                if not word in self.dict_terms:
-                    self.frequency[amount_docs].append(0)
-                    self.dict_terms[word] = amount_terms
-                    amount_terms+=1
-                
-                self.frequency[amount_docs][self.dict_terms[word]] += 1
-
-                max_frequency_terms = max(max_frequency_terms, 
-                    self.frequency[amount_docs][self.dict_terms[word]])
-            
-            maxs.append(max_frequency_terms)
+            VectorModel.__calculate_tf(
+                doc.text,
+                self.dict_terms,
+                self.frequency[amount_docs],
+                self.tfs[amount_docs]
+            )
+            amount_terms = len(self.frequency[amount_docs])
             amount_docs += 1
 
-        for doc in self.frequency:
-            while len(doc) < amount_terms:
-                doc.append(0)
+        for i in range(0, len(self.frequency)):
+            while len(self.frequency[i]) < amount_terms:
+                self.frequency[i].append(0)
+                self.tfs[i].append(0)
 
-        self.tf = [[0.0 for _ in range(0, amount_terms)] 
-                    for _ in range(0, amount_docs)]
-
-        # calculate TF
-        for i in range(0, amount_docs):
-            for j in range(0, amount_terms):
-                tmp = self.frequency[i][j] / maxs[i]
-                self.tf[i][j] = tmp
+        print(self.tfs[amount_docs-1])
 
     def __calculate_idf(self):
         
@@ -72,7 +89,7 @@ class VectorModel(BaseModel):
             self.idf.append(log(amount_docs / n, 2))
 
     def __calculate_weights(self):
-        self.__calculate_tf()
+        self.__calculate_tfs()
         self.__calculate_idf()
 
         self.weights = [[0.0 for _ in range(0, len(self.frequency[0]))] 
@@ -80,4 +97,4 @@ class VectorModel(BaseModel):
 
         for i in range(0, len(self.corpus)):
             for j in range(0, len(self.frequency[0])):
-                self.weights[i][j] = self.tf[i][j] * self.idf[j]
+                self.weights[i][j] = self.tfs[i][j] * self.idf[j]
