4fb6136ee83016fa28e088dedfd3ceec0fe311c2
LRiera24 <laura.riera2412@gmail.com>
2022-11-26 13:31:40 -0500

    Update matches to query

--- src/test.py
+++ src/test.py
@@ -1,11 +1,6 @@
 from sympy.logic.boolalg import to_dnf
 from sympy import sympify
 
-terms = ["a", "b", "c", "d", "e", "f"]
-docs_dict = { 0 : [0, 1, 1, 0, 0, 0],
-              1 : [1, 1, 1, 0, 0, 1],
-              2 : [1, 0, 0, 1, 0, 1]}
-
 def process_query(query):
     # set query to lowercase
     query = query.lower()
@@ -31,47 +26,54 @@ def process_query(query):
         return TypeError("Invalid logical expression.")
 
     # convert query expression into disjunctive normal form, and then convert back to string
-    query_dfn = str(to_dnf(expression))
+    query_dnf = str(to_dnf(expression))
 
     # remove parenthesis from query
-    query_dfn = query_dfn.replace("(", "")
-    query_dfn = query_dfn.replace(")", "")
+    query_dnf = query_dnf.replace("(", "")
+    query_dnf = query_dnf.replace(")", "")
 
     # split by '|' to get conjunctive components (cc)
-    query_dfn = query_dfn.split(" | ")
+    query_dnf = query_dnf.split(" | ")
 
     # split each cc by '&' to get each term in it
-    for i in range(0, len(query_dfn)):
-        query_dfn[i] = query_dfn[i].split(" & ")
+    for i in range(0, len(query_dnf)):
+        query_dnf[i] = query_dnf[i].split(" & ")
 
-    return query_dfn
+    return query_dnf
+
+
+def match_neg_term(term, doc_vector, corpus_terms):
+    for i in range(0, len(doc_vector)):
+        if doc_vector[i] == 1 and corpus_terms[i] == term[1:]:
+            return False
+    return True
+
+
+def match_term(term, doc_vector, corpus_terms):
+    for i in range(0, len(doc_vector)):
+        if doc_vector[i] == 1 and corpus_terms[i] == term:
+            return True
+    return False
 
 
 def doc_matches_cc(cc, doc_vector, corpus_terms):
-    res = 0
+    matches = True
     for term in cc:
-        for i in range(0, len(doc_vector)):
-            if doc_vector[i] == 1:
-                if term[0] == "~":
-                    if corpus_terms[i] == term[1:]:
-                        return False
-                    else:
-                        res += 1
-                elif corpus_terms[i] == term:
-                    res += 1
-                    break
-    if res < len(cc):
-        return False
-    return True
+        if term[0] == '~':
+            matches &= match_neg_term(term, doc_vector, corpus_terms)
+        else:
+            matches &= match_term(term, doc_vector, corpus_terms)
+    return matches
 
 
 def get_docs_matches_to_query(processed_query, docs, corpus_terms):
     matches = []
     for cc in processed_query:
-        for i in range (0, len(docs)):
+        for i in range(0, len(docs)):
             if doc_matches_cc(cc, docs[i], corpus_terms):
                 matches.append(i)
     return matches
+
 
 q = "A AND (B or ~C)"
 q = " ".join(q.split())
@@ -79,6 +81,11 @@ print(q)
 
 expression = process_query(q)
 print(expression)
+
+terms = ["a", "b", "c", "d", "e", "f"]
+docs_dict = { 0 : [0, 1, 1, 0, 0, 0],
+              1 : [1, 1, 1, 0, 0, 1],
+              2 : [1, 0, 0, 1, 0, 1]}
 
 print(terms)
 print(docs_dict)
