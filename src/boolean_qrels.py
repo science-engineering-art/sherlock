760388701bcd302417984747eb61160a8d8c7bd3
Marcos <marcosmath44@gmail.com>
2022-12-17 17:11:08 -0500

    added cranfield_fuzzy.py

--- src/boolean_qrels.py
+++ src/boolean_qrels.py
@@ -1,9 +1,11 @@
 import re
+
 from qrels import QRels
 
 
 class BooleanQRels(QRels):
     def get_query(self, query):
-        query = [ word for word in 
-            re.findall(r"[\w']+", query.text) ]
-        return ' & '.join(query)
+    #     query = [ word for word in 
+    #         re.findall(r"[\w']+", query.text) ]
+    #     return ' & '.join(query)
+        return query
